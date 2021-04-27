20971cc0f9a778685288e795654e2f35ee1ba415
John201858 <39567698+John201858@users.noreply.github.com>
2021-04-27 21:42:24 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+function Header({ name }) {
+  const [flag, setFlag] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Обновляем заголовок документа, используя API браузера
+    document.title = `Вы нажали ${count} раз`;
+  });
+
+  return (
+    <div className="container">
+      <Hello name={name} flag={flag}/>
+      <button onClick={() => {
+        setFlag(!flag);
+        setCount(count + 1);
+        }}>
+        Поменять цвет {+flag}
+      </button>
+    </div>
+  );
+}
+
+function Hello({name, flag}) {
+  return (
+      <h1 className={flag ? "man" : "woman"}>Здравствуйте {name}!</h1>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Header name="Джон"/>
+      <Header name="Джесси"/>
+      <Header name="Джэк"/>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
