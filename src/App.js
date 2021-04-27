f3425724e9c5846f81535a564c7277443c8ebd5b
John201858 <39567698+John201858@users.noreply.github.com>
2021-04-27 22:08:39 +0200

    Добавил строку в компонент Hello

--- src/App.js
+++ src/App.js
@@ -12,30 +12,31 @@ function Header({ name }) {
 
   return (
     <div className="container">
-      <Hello name={name} flag={flag}/>
-      <button onClick={() => {
-        setFlag(!flag);
-        setCount(count + 1);
-        }}>
+      <Hello name={name} flag={flag} />
+      <button
+        onClick={() => {
+          setFlag(!flag);
+          setCount(count + 1);
+          console.log(`${count}`);
+        }}
+      >
         Поменять цвет {+flag}
       </button>
     </div>
   );
 }
 
-function Hello({name, flag}) {
-  return (
-      <h1 className={flag ? "man" : "woman"}>Здравствуйте {name}!</h1>
-  );
+function Hello({ name, flag }) {
+  return <h1 className={flag ? "man" : "woman"}>Здравствуйте {name}!</h1>;
 }
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Header name="Джон"/>
-      <Header name="Джесси"/>
-      <Header name="Джэк"/>
+      <Header name="Джон" />
+      <Header name="Джесси" />
+      <Header name="Джэк" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
