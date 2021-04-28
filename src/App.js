5ffa9ad27f7c43c004a0a47146689b4af15b4af3
John201858 <39567698+John201858@users.noreply.github.com>
2021-04-28 10:15:32 +0300

    Добавил сгенерированный объект

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import { useState, useEffect } from "react";
 import { Button } from "antd";
 import "antd/dist/antd.css";
 import "./styles.css";
+import users from "../users.json";
 
-function Header({ name }) {
-  const [flag, setFlag] = useState(false);
-  const [count, setCount] = useState(0);
+function Header({ user }) {
+  // const [flag, setFlag] = useState(false);
+  // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    // Обновляем заголовок документа, используя API браузера
-    document.title = `Вы нажали ${count} раз`;
+  let users = user.map((user) => {
+    const [flag, setFlag] = useState(false);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      // Обновляем заголовок документа, используя API браузера
+      document.title = `Вы нажали ${count} раз`;
+    });
+
+    return (
+      <div className="container">
+        <Hello name={user.name} flag={flag} />
+        <img src={user.imgUrl} />
+        <p>{user.text}</p>
+        <Button
+          type="primary"
+          onClick={() => {
+            setFlag(!flag);
+            setCount(count + 1);
+          }}
+        >
+          Поменять цвет {+flag}
+        </Button>
+      </div>
+    );
   });
 
-  return (
-    <div className="container">
-      <Hello name={name} flag={flag} />
-      <Button
-        type="primary"
-        onClick={() => {
-          setFlag(!flag);
-          setCount(count + 1);
-        }}
-      >
-        Поменять цвет {+flag}
-      </Button>
-    </div>
-  );
+  return users;
 }
 
 function Hello({ name, flag }) {
@@ -36,10 +46,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Header name="Джон" />
-      <Header name="Джесси" />
-      <Header name="Джэк" />
-      <Header name="Майкл" />
+      <Header user={users} />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
