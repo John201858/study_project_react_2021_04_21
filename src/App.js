97f43bf21cc2a9ce085f851565260067023bb29b
John201858 <39567698+John201858@users.noreply.github.com>
2021-05-14 16:00:07 +0300

    Изменения 14.05.2021, 16.00 Добавил новый компонент "ConvList".

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 // import Message from "./components/Message.js";
-import { useState } from "react";
 import "./styles.css";
 import users from "../users.json";
-import { Conversation, Message } from "./components";
+import { ConvList, Message } from "./components";
 
 export default function App() {
-  const [selection, setSelection] = useState("");
-  const headerClick = (event) => {
-    console.log(event.target.closest("div[id]").id);
-    setSelection(
-      (selection) => (selection = event.target.closest("div[id]").id)
-    );
-  };
-  // setSelection(selection => selection = headerClick());
-
   return (
-    <div className="App" onClick={headerClick}>
+    <div className="App">
+      <ConvList users={users} />
       {/* <Message user={users[0]} />
       <Message user={users[1]} />
       <Message user={users[2]} />
       <Message user={users[3]} /> */}
-      <Conversation user={users[0]} selection={selection} />
+      {/* <Conversation user={users[0]} selection={selection} />
       <Conversation user={users[1]} selection={selection} />
       <Conversation user={users[2]} selection={selection} />
       <Conversation user={users[3]} selection={selection} />
       <Conversation user={users[4]} selection={selection} />
-      <Conversation user={users[5]} selection={selection} />
+      <Conversation user={users[5]} selection={selection} /> */}
     </div>
   );
 }
