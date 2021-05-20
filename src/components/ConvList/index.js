bbbad94bfe3255e315fcf5759b9dfb6bfb8d2027
John201858 <39567698+John201858@users.noreply.github.com>
2021-05-20 16:17:29 +0300

    Изменения 20.05.2021, 16:17

--- src/components/ConvList/index.js
+++ src/components/ConvList/index.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import Conversation from "../Conversation";
+import { Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import 'antd/dist/antd.css';
 import "./ConvList.scss";
 
 export default function ConvList({ users }) {
@@ -12,13 +15,22 @@ export default function ConvList({ users }) {
     );
   };
 
-  const date = users.map((user) => (
+  const data = users.map((user) => (
     <Conversation user={user} selection={selection} />
   ));
 
   return (
-    <div className="ConvList" onClick={headerClick}>
-      {date}
+    <div className="convList" onClick={headerClick}>
+      <div className="convList__header">
+        <div className="convList__header-top">
+          <p>Диалоги ({data.length})</p>
+          <Button className="convList__header-top--button" type="primary">Новый диалог</Button>
+        </div>
+        <div className="convList__header-bottom">
+          <input type="text" placeholder="Поиск..."></input><SearchOutlined />
+        </div>
+      </div>
+      <div className="convList__container">{data}</div>
     </div>
   );
 }
