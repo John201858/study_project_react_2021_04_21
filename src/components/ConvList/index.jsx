332847ce9fc758eb568cf1a661fb7e13e5d8212e
John201858 <39567698+John201858@users.noreply.github.com>
2021-05-26 12:03:08 +0300

    Изменения 26.05.2021, 12:03

--- src/components/ConvList/index.jsx
+++ src/components/ConvList/index.jsx
@@ -7,6 +7,15 @@ import "./ConvList.scss";
 
 export default function ConvList({ users }) {
   const [selection, setSelection] = useState("");
+  const [filtred, setFiltred] = useState(Array.from(users))
+
+  const filtration = value => {
+    setFiltred(
+      users.filter(
+        data => data.name.toLowerCase().indexOf(value.toLowerCase()) >= 0
+      )
+    );
+  }
 
   const headerClick = (event) => {
     console.log(event.target.closest("div[id]").id);
@@ -15,37 +24,37 @@ export default function ConvList({ users }) {
     );
   };
 
-  const data = users.map((user) => (
-    <Conversation
-      user={user}
-      date={user.date}
-      isMe={user.isActive}
-      isOnline={user.isRead}
-      isRead={user.isRead}
-      numbMessage={user.numbMessage}
-      name={user.name}
-      avatar={user.avatar}
-      text={user.text}
-      isNewMessageRead={user.isRead}
-      selection={selection}
-    />
-  ));
-
   return (
     <div className="convList" onClick={headerClick}>
       <div className="convList__header">
         <div className="convList__header-top">
-          <p>Диалоги ({data.length})</p>
+          <p>Диалоги ({filtred.length})</p>
           <Button className="convList__header-top--button" type="primary">
             Новый диалог
           </Button>
         </div>
         <div className="convList__header-bottom">
-          <input type="text" placeholder="Поиск..."></input>
+          <input onChange={event => filtration(event.target.value)} type="text" placeholder="Поиск..."></input>
           <SearchOutlined />
         </div>
       </div>
-      <div className="convList__container">{data}</div>
+      <div className="convList__container">
+        {filtred.map((user) => (
+          <Conversation
+            user={user}
+            date={user.date}
+            isMe={user.isActive}
+            isOnline={user.isRead}
+            isRead={user.isRead}
+            numbMessage={user.numbMessage}
+            name={user.name}
+            avatar={user.avatar}
+            text={user.text}
+            isNewMessageRead={user.isRead}
+            selection={selection}
+          />
+        ))}
+      </div>
     </div>
   );
 }
